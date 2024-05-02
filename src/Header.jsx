08a60f6673f135dd9c3bbc54fcc7a7169fc25400
thomasmsandoval <156626315+thomasmsandoval@users.noreply.github.com>
2024-05-02 14:35:13 -0700

    added link to home

--- src/Header.jsx
+++ src/Header.jsx
@@ -21,9 +21,9 @@ export function Header() {
       <header>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">
+            <Link to="/" className="nav-link active" aria-current="page">
               Trippin
-            </a>
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
